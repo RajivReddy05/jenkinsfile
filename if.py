0c862488e18b73c82d445a56bbf08ff4d42c8361
Rajiv <Rajiv.Anugu@connxai.com>
2021-02-16 14:24:40 +0530

    first commit

---
+++ if.py
@@ -0,0 +1,24 @@
+print("enter")
+name=input()
+name ='RA'
+if name=='RAjiv':
+    print( 'hi RAjiv')
+else:
+    print('wronh')
+    
+print('enter name')
+name= input()
+if name:
+    print('hi')
+else:
+    print('sd')
+    
+spam=10
+while spam<5:
+    print('hiiahia')
+    spam=spam+1
+else:
+    print('b')
+    
+
+    
